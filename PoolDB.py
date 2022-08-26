2c353b7ba034b22fd2cdf8af2738730e3bd70d53
Alex <104856492+OB97@users.noreply.github.com>
2022-08-26 17:02:51 -0300

    Update PoolDB.py

--- PoolDB.py
+++ PoolDB.py
@@ -2,7 +2,7 @@
 # key, name, champs
 
 class PoolDB:
-    def __init__(self, pools={1:["a", 3, "b"], 2:["c",7, "d"], 3:["e",9 , "f"]}):
+    def __init__(self, pools={1:["a", 3, "b"], 2:["c", 7, "d"], 3:["e", 9, "f"]}):
         self.pools = pools
 
     def getPools(self):
