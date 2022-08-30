83e560ba67a9d5a6304b83503bb805d4ac8285c5
Alex <104856492+OB97@users.noreply.github.com>
2022-08-30 12:19:44 -0300

    Update UserDB.py

--- UserDB.py
+++ UserDB.py
@@ -8,18 +8,21 @@ class UserDB:
         i = 0
         usertxt = open("users.txt", "r")
         for line in usertxt:
-            splitted = line.split(",")
-            pools = splitted[1:len(splitted)-1]
-            fin = list(map(int, pools))
-            default = splitted[len(splitted)-1].rstrip()
-            final[i+1] = [splitted[0], fin, int(default)]
-            i = i+1
+            if line is not None:
+                splitted = line.split(",")
+                pools = splitted[1:len(splitted)-1]
+                fin = list(map(int, pools))
+                default = splitted[len(splitted)-1].rstrip()
+                final[i+1] = [splitted[0], fin, int(default)]
+                i = i+1
+            else:
+                next()
         usertxt.close()
         return final
 
     def addUser(self, inp):
         usertxt = open("users.txt", "a")
-        usertxt.writelines("\n" + inp)
+        usertxt.writelines(inp + "\n")
         usertxt.close()
 
     def addUserPool(self, user, inp):
