45f410c1eea806dd02df919e01b6eb7e88c5f673
OB97 <ajobrien@upei.ca>
2023-07-12 09:58:44 -0300

    Revamp statistic organization following YouTube example for processing JSON

--- GrabData.py
+++ GrabData.py
@@ -1,9 +1,22 @@
 # Program to connect with League of Legends API and pull the data into a spreadsheet
 # By: Alex O'Brien
 
-def main(){
-  return 0
-}
+import requests
+import json
+import pprint
+
+API_KEY = "RGAPI-2385d6ea-9a68-4b03-be76-30b771b3e843"
+DATA_URL = "http://ddragon.leagueoflegends.com/cdn/13.13.1/data/en_US/champion.json"
+
+
+def main():
+    response = requests.get(DATA_URL)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        pprint.pprint(data['data'])
+    else:
+        print(f"Error: {response.status_code}")
+
 
 if __name__ == "__main__":
-  main()
+    main()
