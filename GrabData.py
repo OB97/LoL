b0557c59f0f42f2add065c042a55d0f4f924d036
OB97 <ajobrien@upei.ca>
2023-07-26 09:53:12 -0300

    Separated into main and utility

--- GrabData.py
+++ GrabData.py
@@ -3,19 +3,17 @@
 
 import requests
 import json
-import pprint
 
-DATA_URL = "http://ddragon.leagueoflegends.com/cdn/13.13.1/data/en_US/champion.json"
+_DATA_URL = "http://ddragon.leagueoflegends.com/cdn/13.13.1/data/en_US/champion.json"
 
 
-def main():
-    response = requests.get(DATA_URL)
+# Function to retrieve and parse json data into a dictionary
+# @params: none
+# @return: dictionary
+def getData():
+    response = requests.get(_DATA_URL)
     if response.status_code == 200:
         data = json.loads(response.text)
-        pprint.pprint(data['data'])
+        return data['data']
     else:
         print(f"Error: {response.status_code}")
-
-
-if __name__ == "__main__":
-    main()
---
+++ Main.py
@@ -0,0 +1,12 @@
+# Main Project Hub
+# By: Alex O'Brien
+import GrabData
+import pprint
+
+
+def main():
+    pprint.pprint(GrabData.getData())
+
+
+if __name__ == "__main__":
+    main()
